51c687662a180463691f8909f655f21f5c1dbec1
Kumar <futurestrading114@gmail.com>
2026-05-10 23:08:40 -0700

    Add backend root health route

--- backend/routes/serviceRoutes.js
+++ backend/routes/serviceRoutes.js
@@ -1,6 +1,7 @@
 import { health, listServices } from '../controllers/serviceController.js'
 
 export const serviceRoutes = [
+  { method: 'GET', path: '/', handler: health },
   { method: 'GET', path: '/api/health', handler: health },
   { method: 'GET', path: '/api/services', handler: listServices },
 ]
